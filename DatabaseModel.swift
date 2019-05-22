3fdcc43022ccf4a8710eef8d169c065c66ffdd8d
Meet1295 <50322526+Meet1295@users.noreply.github.com>
2019-05-22 10:12:33 +0530

    Add files via upload

---
+++ DatabaseModel.swift
@@ -0,0 +1,18 @@
+//
+//  DatabaseModel.swift
+//  MyApp
+//
+//  Created by Admin on 10/1/18.
+//  Copyright © 2018 Olynk Inc. All rights reserved.
+//
+
+import Foundation
+
+class DBModel: NSObject {
+    var targetId = String()
+    var rssi = String()
+    var timestamp = String()
+    var latitude = String()
+    var longitude = String()
+    var betteryStatus = String()
+}
